18d7d5acb7a99cafb7c6066577ef6f4eb95e34eb
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-09-14 00:12:31 +0300

    create server

---
+++ server.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const app = new Koa();
+const Router = require('koa-router');
+const json = require('koa-json');
+const router = new Router();
+const uuid = require('uuid');
+var slow = require('koa-slow');
+
+app.use( koaBody({
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
+app.use(json());
+
+app.use(slow({
+  url: /getdata$/i,
+  delay: 10000
+}))
+
+app.use( async (ctx, next) => {
+  const origin = ctx.request.get('Origin');
+  if (!origin) {
+    return await next();
+  }
+
+  const headers = { 'Access-Control-Allow-Origin': '*' };
+
+  if (ctx.request.method !== 'OPTIONS') {
+    console.log('! OPTIONS');
+    ctx.response.set({ ...headers });
+    try {
+      return await next();
+    } catch (e) {
+      e.headers = { ...e.headers, ...headers };
+      throw e;
+    }
+  }
+
+  if (ctx.request.get('Access-Control-Request-Method')) {
+    ctx.response.set({
+      ...headers,
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+    });
+
+    if (ctx.request.get('Access-Control-Request-Headers')) {
+      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
+    }
+    ctx.response.status = 204;
+  }
+})
+
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback());
+
+router.get('/getdata', (ctx) => {
+  console.log('запрос получен')
+  ctx.response.status = 404;
+  console.log('запрос отправлен')
+})
+
+
+app.use(router.routes()).use(router.allowedMethods());
+
+server.listen(port);
